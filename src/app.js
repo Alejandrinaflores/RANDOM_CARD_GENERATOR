56c4ebedd8ee724c8082628f9fb6a6e7aa58626a
Alejandrinaflores <floresyunny@gmail.com>
2022-08-04 20:35:29 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let cardCharacters = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+  let cardSymbols = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function newCard() {
+    let characterRandom = Math.floor(Math.random() * cardCharacters.length);
+    let symbolRandom = Math.floor(Math.random() * cardSymbols.length);
+
+    document.getElementById("characters").innerHTML =
+      cardCharacters[characterRandom];
+    document.getElementById("symbolUp").innerHTML = cardSymbols[symbolRandom];
+    document.getElementById("symbolDown").innerHTML = cardSymbols[symbolRandom];
+
+    if (cardSymbols[symbolRandom] == "&diams;") {
+      document.getElementById("symbolUp").style.color = "red";
+      document.getElementById("symbolDown").style.color = "red";
+    } else if (cardSymbols[symbolRandom] == "&hearts;") {
+      document.getElementById("symbolUp").style.color = "red";
+      document.getElementById("symbolDown").style.color = "red";
+    } else {
+      document.getElementById("symbolUp").style.color = "black";
+      document.getElementById("symbolDown").style.color = "black";
+    }
+  }
+
+  setInterval(() => {
+    newCard();
+  }, 3000);
 };
